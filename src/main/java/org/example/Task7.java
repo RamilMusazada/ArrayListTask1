2230cd179ef653dc1db55826b5d63187a7740077
RamilMusazada <themusazadeh@gmail.com>
2024-11-21 00:01:48 +0400

    Tamamlanmayib

---
+++ src/main/java/org/example/Task7.java
@@ -0,0 +1,15 @@
+package org.example;
+
+import java.util.LinkedList;
+
+//Bir LinkedList yaradın və bir neçə rəqəm əlavə edin.
+//Sonuncu elementi birinci elementə qoşaraq (loop yaradaraq) siyahını dövrə salın.
+//Düzgün işlədiyini yoxlamaq üçün dövr qurub hər elementi ekrana çıxarın (dövr pozulana qədər)
+public class Task7 {
+    public static void main(String[] args) {
+        LinkedList <Integer> list = new LinkedList<>();
+        list.add(1);
+        list.add(2);
+        list.add(7);
+    }
+}
